b5f081d74b74dbefc54c1a21072d68ed72768fb8
Marcel <suterma@users.noreply.github.com>
2018-03-13 21:46:44 +0000

    Handling some Exception nicer
    Exceptions when no mp3 playing is available catched and presented with thext.

---
+++ SOURCE/Replayer.WinForms.Ui/Components/ErrorBox.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Replayer.WinForms.Ui.Gui
+{
+    partial class ErrorBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErrorBox));
+            this.okPanel1 = new Replayer.WinForms.Ui.Gui.OkPanel();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // okPanel1
+            // 
+            this.okPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.okPanel1.Location = new System.Drawing.Point(0, 81);
+            this.okPanel1.MinimumSize = new System.Drawing.Size(156, 78);
+            this.okPanel1.Name = "okPanel1";
+            this.okPanel1.Size = new System.Drawing.Size(416, 78);
+            this.okPanel1.TabIndex = 0;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.labelControl1.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.labelControl1.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            this.labelControl1.Appearance.TextOptions.WordWrap = DevExpress.Utils.WordWrap.Wrap;
+            this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.Vertical;
+            this.labelControl1.Location = new System.Drawing.Point(0, 10);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(416, 13);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "Wassup?";
+            // 
+            // ErrorBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 159);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.okPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ErrorBox";
+            this.Text = "Error";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OkPanel okPanel1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
---
+++ SOURCE/Replayer.WinForms.Ui/Components/ErrorBox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Replayer.WinForms.Ui.Gui {
+    /// <summary>
+    ///     Displays an Error Message.
+    /// </summary>
+    public partial class ErrorBox : XtraForm {
+        /// <summary>
+        ///     Prepares the message.
+        /// </summary>
+        /// <param name="message"></param>
+        public ErrorBox(string message, string title = "") {
+            InitializeComponent();
+            labelControl1.Text = message;
+            this.Text = title;
+        }
+
+        /// <summary>
+        ///     Shows the message and returns a DialogResult.
+        /// </summary>
+        /// <returns></returns>
+        public DialogResult Show() {
+            return ShowDialog();
+        }
+
+        public static DialogResult Show(string message)
+        {
+            return new ErrorBox(message).Show();
+        }
+
+        internal static DialogResult Show(string message, string title)
+        {
+            return new ErrorBox(message, title).Show();
+        }
+    }
+}
