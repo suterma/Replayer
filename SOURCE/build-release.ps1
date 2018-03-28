139e0da123bb620ebc1c98842376bcbdcd246c36
Marcel <suterma@users.noreply.github.com>
2018-03-28 17:01:42 +0200

    Added draft of build script

--- SOURCE/build-release.ps1
+++ SOURCE/build-release.ps1
@@ -1,2 +1,16 @@
+# -----------------------------------------------------
+# Builds and signs a release of Replayer.exe
+# Run from the Developer Command Prompt for VS 2017
+# -----------------------------------------------------
+
+#go to project
+#cd \GitHub\Replayer
+
 # creates a release build, ready for deployment
-C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\MsBuild.exe Replayer.Application.sln /t:Build /p:Configuration=Release /p:TargetFramework=v4.0
+msbuild \SOURCE\Replayer.Application.sln /t:Build /p:Configuration=Release /p:TargetFramework=v4.0
+
+# Copy the binary over to release
+xcopy .\SOURCE\Replayer.WinForms.Ui\bin\Release\Replayer.exe .\RELEASE\ /V /F /R /Y 
+
+# signing
+signtool sign /a \SOURCE\Release\Replayer.exe
