d00b78f5ad5327c919c843098a20d79fbf7e0f3a
Marcel <marcel+github@codeministry.ch>
2018-03-28 23:56:47 +0200

    Added splash screen

---
+++ SOURCE/Replayer.WinForms.Ui/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+
+namespace Replayer.WinForms.Ui {
+    /// <summary>
+    /// A startup splash form
+    /// </summary>
+    /// <seealso cref="Microsoft.VisualBasic.ApplicationServices.WindowsFormsApplicationBase" />
+    public class Startup : WindowsFormsApplicationBase {
+        /// <summary>
+        /// When overridden in a derived class, allows a designer to emit code that initializes the splash screen.
+        /// </summary>
+        protected override void OnCreateSplashScreen() {
+            SplashScreen = new SplashForm();
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, allows a designer to emit code that configures the splash screen and main form.
+        /// </summary>
+        protected override void OnCreateMainForm() {
+            MainForm = new ReplayerApplicationForm();
+        }
+    }
+}
