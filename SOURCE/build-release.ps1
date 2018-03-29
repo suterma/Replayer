55de3627ac390b2a66ae178396252085ffffe845
Marcel <suterma@users.noreply.github.com>
2018-03-29 16:45:33 +0200

    Added signing to build script

--- SOURCE/build-release.ps1
+++ SOURCE/build-release.ps1
@@ -1,6 +1,7 @@
 # -----------------------------------------------------
 # Builds and signs a release of Replayer.exe
 # Run from the Developer Command Prompt for VS 2017
+# See https://docs.microsoft.com/en-us/dotnet/framework/tools/signtool-exe
 # -----------------------------------------------------
 
 #go to project
@@ -13,4 +14,6 @@ msbuild \SOURCE\Replayer.Application.sln /t:Build /p:Configuration=Release /p:Ta
 xcopy .\SOURCE\Replayer.WinForms.Ui\bin\Release\Replayer.exe .\RELEASE\ /V /F /R /Y 
 
 # signing
-signtool sign /a \SOURCE\Release\Replayer.exe
+signtool sign /a /v \SOURCE\Release\Replayer.exe
+
+signtool verify \SOURCE\Release\Replayer.exe
